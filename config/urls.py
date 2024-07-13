c37fa4727f9f6bc2d40b5f6c69af55640057349e
JefferDevop <somadesarrollo@gmail.com>
2024-07-13 17:59:27 -0500

    Dockerfile + all

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,7 @@
-"""core URL Configuration
+"""web URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.0.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,17 +13,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
 
 from config import settings
-# from core.dashboard.views import *
+from core.dashboard.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('dashboard/', DashboardView.as_view(), name='dashboard'),
+    path('login/', include('core.login.urls')),
+    path('pos/', include('core.pos.urls')),
+    path('reports/', include('core.reports.urls')),
+    path('rrhh/', include('core.rrhh.urls')),
+    path('security/', include('core.security.urls')),
+    path('tenant/', include('core.tenant.urls')),
+    path('user/', include('core.user.urls')),
+    path('', DashboardView.as_view(), name='dashboard'),
 ]
 
-
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
