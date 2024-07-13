debd3053281a62d9e771571c243a13a310f5c344
JefferDevop <somadesarrollo@gmail.com>
2024-07-12 23:02:45 -0500

    My first commit

--- config/settings.py
+++ config/settings.py
@@ -165,3 +165,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DOMAIN = env.str('DOMAIN', default='localhost')
