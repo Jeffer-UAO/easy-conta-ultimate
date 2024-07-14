d326a530a36d8db2effafc87f6efa3e07583f7fc
JefferDevop <somadesarrollo@gmail.com>
2024-07-14 00:11:05 -0500

    Dockerfile + app + collecstatic+ whitenoise

--- config/settings.py
+++ config/settings.py
@@ -46,6 +46,8 @@ TENANT_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',   
     'core.security',
     'core.user',
     'core.login',
@@ -183,7 +185,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATIC_URL = '/static/'
 
