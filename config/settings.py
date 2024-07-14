d3c3f2ccd2bbc8f05963040382b4c8a2e781a34b
JefferDevop <somadesarrollo@gmail.com>
2024-07-14 17:21:14 -0500

    Dockerfile + app + collecstatic+ whitenoise

--- config/settings.py
+++ config/settings.py
@@ -192,12 +192,12 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Auth
 
