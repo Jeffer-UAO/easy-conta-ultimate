93abc01becf45d92c437b738b1ca9b5f07df16a8
JefferDevop <somadesarrollo@gmail.com>
2024-07-13 23:13:54 -0500

    Dockerfile + app + collecstatic+ whitenoise

--- config/urls.py
+++ config/urls.py
@@ -33,5 +33,7 @@ urlpatterns = [
     path('', DashboardView.as_view(), name='dashboard'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
